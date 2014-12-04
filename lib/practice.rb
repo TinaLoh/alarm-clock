0b94f062ef33a25603cb67da96422d84a682623e
TinaLoh <tinalola@gmail.com>
2014-12-03 19:41:37 -0800

    add code

---
+++ lib/practice.rb
@@ -0,0 +1,15 @@
+
+start_time = Time.now
+
+while true
+  if Time.now - start_time == 10
+    system("say \"times up\"")
+    exit
+  end
+end
+
+
+# while true
+# break if Time.now - start_time == 10
+# end
+#system("say\"times up\"")
